7a151a30943835178ca521c6b8964544501dd18d
Giao Ho <joutvhu@gmail.com>
2023-09-20 02:33:03 +0700

    Init plugin

---
+++ src/main/java/com/github/joutvhu/jenkins/multibranch/triggers/ExtendedWorkflowMultiBranchProject.java
@@ -0,0 +1,36 @@
+package com.github.joutvhu.jenkins.multibranch.triggers;
+
+import hudson.Extension;
+import hudson.model.Item;
+import hudson.model.Run;
+import hudson.model.listeners.ItemListener;
+import hudson.model.listeners.RunListener;
+import org.jenkinsci.plugins.workflow.multibranch.WorkflowBranchProjectFactory;
+
+public class ExtendedWorkflowMultiBranchProject extends WorkflowBranchProjectFactory {
+    @Extension
+    public static class ItemListenerImpl extends ItemListener {
+        @Override
+        public void onCreated(Item item) {
+            super.onCreated(item);
+            PipelineTriggerProperty.triggerPipelineTriggerPropertyFromParentForOnCreate(item);
+        }
+
+        @Override
+        public void onDeleted(Item item) {
+            super.onDeleted(item);
+            PipelineTriggerProperty.triggerPipelineTriggerPropertyFromParentForOnDelete(item);
+        }
+
+    }
+
+    @Extension
+    public static class RunListenerImpl extends RunListener<Run>
+    {
+        @Override
+        public void onDeleted(Run run) {
+            super.onDeleted(run);
+            PipelineTriggerProperty.triggerPipelineTriggerPropertyFromParentForOnRunDelete(run);
+        }
+    }
+}
---
+++ src/main/java/com/github/joutvhu/jenkins/multibranch/triggers/PullRequestInfo.java
@@ -0,0 +1,21 @@
+package com.github.joutvhu.jenkins.multibranch.triggers;
+/**
+ * Class for storing Pull Request source and target branch names
+ */
+public class PullRequestInfo {
+    private String sourceBranchName;
+    private String targetBranchName;
+
+    public PullRequestInfo(String sourceBranchName, String targetBranchName) {
+        this.sourceBranchName = sourceBranchName;
+        this.targetBranchName = targetBranchName;
+    }
+
+    public String getSourceBranchName() {
+        return sourceBranchName;
+    }
+
+    public String getTargetBranchName() {
+        return targetBranchName;
+    }
+}
